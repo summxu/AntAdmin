2a9037f381438c3e89cf54cecff919ba8dcf3951
Chenxu <chenxu@foxmai.com>
2020-01-07 16:36:38 +0800

    修改登录信息

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,7 +1,7 @@
 /*
  * @Auth: Chenxu
  * @Date: 2019-12-10 16:14:47
- * @LastEditTime : 2020-01-07 16:34:05
+ * @LastEditTime : 2020-01-07 16:36:24
  */
 import request from '@/utils/request';
 
@@ -13,7 +13,7 @@ export interface LoginParamsType {
 }
 
 export async function fakeAccountLogin(params: LoginParamsType) {
-  return request('/api/login', {
+  return request('/api/login/account', {
     method: 'POST',
     data: params,
   });
